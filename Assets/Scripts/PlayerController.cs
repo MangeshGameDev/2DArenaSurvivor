e1e6dd3a56c349fc6e16ac0bb47d33e8b078fefb
MangeshGameDev <mkmangesh0007@gmail.com>
2025-06-19 17:11:02 +0530

    1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   private float moveSpeed = 5f;
+   private float sprintSpeed = 10f;
+
+    private void Update()
     {
-        
+        Movement();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Movement()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical,0.0f );
+        movement.Normalize(); // Normalize to prevent faster diagonal movement
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(movement * sprintSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(movement * moveSpeed * Time.deltaTime);
+        }
     }
 }
